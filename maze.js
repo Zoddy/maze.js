7b1c51c6245c64f0c392b2f552a873609da1ab05
André Kussmann <zoddy@zoddy.de>
2017-04-30 13:58:31 +0200

    more modern code and standard code style

--- maze.js
+++ maze.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * base function for creating a maze
@@ -6,25 +6,22 @@
  * @param {number} sizeX count of rooms on the x axis
  * @param {number} sizeY count of rooms on the y axis
  */
-var maze = function(sizeX, sizeY) {
-  maze._directions = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}];
-  maze._links = {};
-  maze._path = [];
-  maze._size = {
-    x: sizeX,
-    y: sizeY
-  };
-  maze._visitCount = 0;
-  maze._visited = {};
+const maze = function(sizeX, sizeY) {
+  maze._directions = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}]
+  maze._links = {}
+  maze._path = []
+  maze._size = {x: sizeX, y: sizeY}
+  maze._visitCount = 0
+  maze._visited = {}
 
   // create maze with startpoint
   maze._create({
     x: maze._random(1, maze._size.x),
     y: maze._random(1, maze._size.y)
-  });
+  })
 
-  return maze._links;
-};
+  return maze._links
+}
 
 
 /**
@@ -33,31 +30,29 @@ var maze = function(sizeX, sizeY) {
  * @param {object} room next room to visit and search, with x- and y-coords
  */
 maze._create = function(room) {
-  var nextRoom;
-
   // add room to visited rooms and to the path
-  if (maze._visited[room.x + ',' + room.y] === undefined) {
-    maze._visited[room.x + ',' + room.y] = true;
-    ++maze._visitCount;
+  if (maze._visited[`${room.x},${room.y}`] === undefined) {
+    maze._visited[`${room.x},${room.y}`] = true
+    ++maze._visitCount
   }
 
   // get next random room
-  nextRoom = maze._getDirection(room);
+  const nextRoom = maze._getDirection(room)
 
   if (nextRoom === false) {
     if (maze._visitCount < maze._size.x * maze._size.y) {
       // ok, we are stucked, but don't visited all rooms
-      maze._create(maze._path.pop());
+      maze._create(maze._path.pop())
     }
   } else {
-    maze._path.push(room); // add room to path
-    maze._links[room.x + ',' + room.y + '_' + nextRoom.x + ',' + nextRoom.y] = [
+    maze._path.push(room) // add room to path
+    maze._links[`${room.x},${room.y}_${nextRoom.x},${nextRoom.y}`] = [
       room,
       nextRoom
-    ];
-    maze._create(nextRoom); // go to next room
+    ]
+    maze._create(nextRoom) // go to next room
   }
-};
+}
 
 
 /**
@@ -67,42 +62,41 @@ maze._create = function(room) {
  * @return {object|boolean} returns the next room or false, if we are stucked
  */
 maze._getDirection = function(room) {
-  var directions = [],
-      direction,
-      nextRoom;
+  const directions = []
+  let nextRoom = {}
 
   // can we add north?
-  if (room.y !== 1 && !maze._visited[room.x + ',' + (room.y - 1)]) {
-    directions.push(1);
+  if (room.y !== 1 && !maze._visited[`${room.x},${room.y - 1}`]) {
+    directions.push(1)
   }
 
   // can we add east?
-  if (room.x !== maze._size.x && !maze._visited[(room.x + 1) + ',' + room.y]) {
-    directions.push(2);
+  if (room.x !== maze._size.x && !maze._visited[`${room.x + 1},${room.y}`]) {
+    directions.push(2)
   }
 
   // can we add south?
-  if (room.y !== maze._size.y && !maze._visited[room.x + ',' + (room.y + 1)]) {
-    directions.push(3);
+  if (room.y !== maze._size.y && !maze._visited[`${room.x},${room.y + 1}`]) {
+    directions.push(3)
   }
 
   // can we add west?
-  if (room.x !== 1 && !maze._visited[(room.x - 1) + ',' + room.y]) {
-    directions.push(4);
+  if (room.x !== 1 && !maze._visited[`${room.x - 1},${room.y}`]) {
+    directions.push(4)
   }
 
   if (directions.length === 0) {
-    nextRoom = false;
+    nextRoom = false
   } else {
-    direction = directions[maze._random(0, directions.length - 1)] - 1;
+    const direction = directions[maze._random(0, directions.length - 1)] - 1
     nextRoom = {
       x: room.x + maze._directions[direction].x,
       y: room.y + maze._directions[direction].y
-    };
+    }
   }
 
-  return nextRoom;
-};
+  return nextRoom
+}
 
 
 /**
@@ -112,7 +106,7 @@ maze._getDirection = function(room) {
  * @param {number} max maximum number
  */
 maze._random = function(min, max) {
-  return min + parseInt(Math.random() * (max - min + 1), 10);
-};
+  return min + parseInt(Math.random() * (max - min + 1), 10)
+}
 
-module.exports = maze;
+module.exports = maze
